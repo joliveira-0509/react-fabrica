3bc4f2670d1e591860d9fee0a4b546b95ebd4fe5
João <joaovitorsilvadeoliveira82@gmail.com>
2025-01-27 11:04:10 -0400

    comit inicial

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,64 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="body">
+      <div id="menuBar">
+        <img id="menuIcon" src="img/img.jpg" alt="Menu Icon" />
+      </div>
+      <section className="slider">
+        <div className="slider-content">
+          <input type="radio" name="btn-radio" id="radio1" />
+          <input type="radio" name="btn-radio" id="radio2" />
+          <input type="radio" name="btn-radio" id="radio3" />
+          <div className="slide-box primeiro">
+            <img className="img-desktop" src="img/mk.jpg" alt="slide1" />
+          </div>
+          <div className="slide-box">
+            <img className="img-desktop" src="img/mk2.jpg" alt="slide1" />
+          </div>
+          <div className="slide-box">
+            <img className="img-desktop" src="img/mk3.jpg" alt="slide1" />
+          </div>
+          <div className="nav-auto">
+            <div className="auto-btn1" />
+            <div className="auto-btn2" />
+            <div className="auto-btn3" />
+          </div>
+          <div className="nav-manual">
+            <label htmlFor="radio1" className="manual-btn" />
+            <label htmlFor="radio2" className="manual-btn" />
+            <label htmlFor="radio3" className="manual-btn" />
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+      </section>
+      <div className="div-box">
+        <a className="cartao , a" href="eventos.html">
+          <img src="img/img.jpg" alt />
+          <h1>Eventos</h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et
+            dolore magna aliqua</h2>
         </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+        <a className="cartao , a" href="atendimento.html">
+          <img src="img/img.jpg" alt />
+          <h1>Atendimentos</h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua</h2>
         </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <a className="cartao , a" href="sobre.html">
+          <img src="img/img.jpg" alt />
+          <h1>Nos Conheça</h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua</h2>
         </a>
-      </footer>
+        <a className="cartao , a" href="outro.html">
+          <img src="img/img.jpg" alt />
+          <h1>Outros meios</h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua</h2>
+        </a>
+      </div>
     </div>
+
   );
 }
