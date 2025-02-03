b4c3130121b8123b220e0c6df15a46f4597a36a2
João <joaovitorsilvadeoliveira82@gmail.com>
2025-02-03 11:29:26 -0400

    carrossel

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,72 @@
 import React from 'react';
-import "./page.module.css";
-import "./page.slide.module.css";
-import img from './img/img.jpg';
+import style from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Home() {
+
+
   return (
-<div className="body">
-  <section className="slider">
-    <div className="slider-content">
-      <input type="radio" name="btn-radio" id="radio1" />
-      <input type="radio" name="btn-radio" id="radio2" />
-      <input type="radio" name="btn-radio" id="radio3" />
-      <div className="slide-box primeiro">
-        <img className="img-desktop" src="img/mk.jpg" alt="slide1" />
-      </div>
-      <div className="slide-box">
-        <img className="img-desktop" src="img/mk2.jpg" alt="slide1" />
-      </div>
-      <div className="slide-box">
-        <img className="img-desktop" src="img/mk3.jpg" alt="slide1" />
-      </div>
-      <div className="nav-auto">
-        <div className="auto-btn1" />
-        <div className="auto-btn2" />
-        <div className="auto-btn3" />
-      </div>
-      <div className="nav-manual">
-        <label htmlFor="radio1" className="manual-btn" />
-        <label htmlFor="radio2" className="manual-btn" />
-        <label htmlFor="radio3" className="manual-btn" />
+    <div>
+      <div className={style.body}>
+
+        <div style={{ display: 'block', width: 700, padding: 30 }}>
+          <h4>React-Bootstrap Carousel Component</h4>
+          <Carousel fade>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                alt="Image One"
+              />
+              <Carousel.Caption>
+                <h3>Label for first slide</h3>
+                <p>Sample Text for Image One</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+              <img
+                className="d-block w-100"
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+                alt="Image Two"
+              />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+
+        <div className={style.div_box}>
+          <a className={style.cartao} href="eventos.html">
+            <img src="{img}" alt />
+            <h1>Eventos</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+              labore et
+              dolore magna aliqua</h2>
+          </a>
+          <a className={style.cartao} href="atendimento.html">
+            <img src="img/img.jpg" alt />
+            <h1>Atendimentos</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua</h2>
+          </a>
+          <a className={style.cartao} href="sobre.html">
+            <img src="img/img.jpg" alt />
+            <h1>Nos Conheça</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua</h2>
+          </a>
+          <a className={style.cartao} href="outro.html">
+            <img src="img/img.jpg" alt />
+            <h1>Outros meios</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua</h2>
+          </a>
+        </div>
       </div>
     </div>
-  </section>
-  <div className="div-box">
-    <a className="cartao" href="eventos.html">
-      <img src={img} alt />
-      <h1>Eventos</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-        labore et
-        dolore magna aliqua</h2>
-    </a>
-    <a className="cartao" href="atendimento.html">
-      <img src="img/img.jpg" alt />
-      <h1>Atendimentos</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua</h2>
-    </a>
-    <a className="cartao" href="sobre.html">
-      <img src="img/img.jpg" alt />
-      <h1>Nos Conheça</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua</h2>
-    </a>
-    <a className="cartao" href="outro.html">
-      <img src="img/img.jpg" alt />
-      <h1>Outros meios</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua</h2>
-    </a>
-  </div>
-</div>
 
   );
 }
